82c982fa63fdfb5158d21495bce58342df4ca603
juliokhichfy <juliocesark@gmail.com>
2019-01-22 11:26:47 -0200

    Listagem, delete, busca e paginação da Entidade Categoria

--- src/main/java/com/julio/springboot/services/CategoriaService.java
+++ src/main/java/com/julio/springboot/services/CategoriaService.java
@@ -1,12 +1,18 @@
 package com.julio.springboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julio.springboot.domain.Categoria;
 import com.julio.springboot.repositories.CategoriaRepository;
+import com.julio.springboot.services.exceptions.DataIntegrityException;
 import com.julio.springboot.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +36,25 @@ public class CategoriaService {
 		Categoria newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	private void updateData(Categoria newObj, Categoria obj) {
